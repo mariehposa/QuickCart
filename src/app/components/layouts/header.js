3fcee71cd55871f84e348fc206942a25bc426289
mariehposa <mariamadedeji.work@gmail.com>
2024-02-02 00:15:28 +0000

    update header

--- src/app/components/layouts/header.js
+++ src/app/components/layouts/header.js
@@ -3,21 +3,21 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="flex items-center justify-between">
-      <Link className="text-primary font-bold text-3xl" href={""}>
-        FlavourDash
+      <Link className="text-primary font-bold text-5xl" href={""}>
+        GrocHub
       </Link>
       <nav className="flex gap-8 text-link font-medium items-center">
         <Link href={""}>Home</Link>
         <Link href={""}>Menu</Link>
         <Link href={""}>About</Link>
         <Link href={""}>Contact</Link>
-        <Link
-          className="bg-primary text-white px-6 py-2 rounded-full ml-6"
-          href={""}
-        >
-          Login
-        </Link>
       </nav>
+      <Link
+        className="bg-primary text-white px-6 py-2 rounded-full ml-6"
+        href={""}
+      >
+        Login
+      </Link>
     </header>
   );
 };
