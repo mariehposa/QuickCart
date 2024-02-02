aa1ad4afd814d8f7ef1453a7d4b1339d5088c5b4
mariehposa <mariamadedeji.work@gmail.com>
2024-02-02 00:21:11 +0000

    create hero section

--- src/app/components/layouts/Hero.js
+++ src/app/components/layouts/Hero.js
@@ -1,17 +1,37 @@
 import Image from "next/image";
+import RightIcon from "../icons/right";
 
 const Hero = () => {
   return (
-    <section>
-      <h1>Explore. Savor. Enjoy.</h1>
-      <p>
-        Discover a world of culinary wonders right at your fingertips. From
-        mouthwatering Italian pastas to fiery Indian curries, and everything in
-        between, FlavorDash offers an unparalleled selection that promises to
-        tantalize your taste buds and ignite your senses.
-      </p>
-      <div className="w-full h-max">
-        <Image src={"/wings.jpg"} alt="" layout="fill" objectFit="contain" />
+    <section className="flex justify-between my-8">
+      <div className="w-1/3 ">
+        <h1 className="text-4xl font-semibold mb-7">
+          Freshness Delivered, Fast and Easy.
+        </h1>
+        <p className="text-lg">
+          Tired of the hassle of grocery shopping? Say goodbye to long lines and
+          crowded aisles – GrocHub is here to revolutionize your shopping
+          experience. With just a few clicks, you can order all your groceries
+          from the comfort of your home and have them delivered right to your
+          doorstep.
+        </p>
+        <div className="flex gap-8 mt-10">
+          <button className="bg-primary text-white px-6 py-2 rounded-full flex gap-2 items-center">
+            Order now <RightIcon className="w-4 h-4" />
+          </button>
+          <button className="flex gap-2 items-center">
+            Learn more <RightIcon />
+          </button>
+        </div>
+      </div>
+      <div className="w-2/4">
+        <Image
+          src="/grocery.jpg"
+          alt="groceries"
+          layout="fill"
+          objectFit="contain"
+          className="!relative rounded"
+        />
       </div>
     </section>
   );
