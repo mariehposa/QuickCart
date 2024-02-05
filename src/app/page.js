07b03815e9add9a3b8f2b212f1c54d27f78adeeb
mariehposa <mariamadedeji.work@gmail.com>
2024-02-05 16:29:33 +0000

    import menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
+import Menu from "./components/layout/Menu";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Menu />
     </>
   );
 }
