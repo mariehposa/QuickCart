de7ce46da0318c72d249d2f7892cc674262a32ed
mariehposa <mariamadedeji.work@gmail.com>
2024-02-06 13:36:45 +0000

    render items

--- src/app/components/layout/Menu.js
+++ src/app/components/layout/Menu.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import MenuItem from "../menu/MenuItem";
 import SectionHeaders from "./SectionHeaders";
 
 const Menu = () => {
@@ -6,10 +7,20 @@ const Menu = () => {
     <section>
       <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
-          <Image src={"/left-salad.png"} width={109} height={189} alt={"salad"} />
+          <Image
+            src={"/left-salad.png"}
+            width={109}
+            height={189}
+            alt={"salad"}
+          />
         </div>
         <div className="absolute -top-[70px] right-0 -z-10">
-          <Image src={"/right-salad.png"} width={107} height={195} alt={"salad"} />
+          <Image
+            src={"/right-salad.png"}
+            width={107}
+            height={195}
+            alt={"salad"}
+          />
         </div>
       </div>
       <div className="text-center mb-4 mt-16">
@@ -17,6 +28,14 @@ const Menu = () => {
           subHeader={"~ special products ~"}
           mainHeader={"Our Best Sellers"}
         />
+      </div>
+      <div className="flex flex-wrap gap-16 justify-center">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </section>
   );
