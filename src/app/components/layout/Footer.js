9d78c9abdf4951d043866d9c884dd0d2e1ee7346
mariehposa <mariamadedeji.work@gmail.com>
2024-02-06 15:06:01 +0000

    create footer

---
+++ src/app/components/layout/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="border-t p-8 text-center text-gray-500 mt-16">
+      &copy; 2024 All rights reserved
+    </footer>
+  );
+};
+
+export default Footer;
