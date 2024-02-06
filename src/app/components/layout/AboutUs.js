9be36d16cdd89da7f1c5466485e2e6bf2a58de7b
mariehposa <mariamadedeji.work@gmail.com>
2024-02-06 15:05:28 +0000

    implement about section

---
+++ src/app/components/layout/AboutUs.js
@@ -0,0 +1,30 @@
+import SectionHeaders from "./SectionHeaders";
+
+const AboutUs = () => {
+  return (
+    <section className="text-center my-16" id="about">
+      <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+      <div className="text-gray-500 max-w-lg mx-auto mt-4 flex flex-col gap-4">
+        <p>
+          At GrocHub, we are passionate about simplifying the way you shop for
+          groceries. We understand the challenges and time constraints that come
+          with modern-day living, which is why we have made it our mission to
+          provide you with a seamless and convenient solution for all your
+          grocery needs.
+        </p>
+        <p>
+          GrocHub was born out of a desire to revolutionize the traditional
+          grocery shopping experience. We are also proud to offer fast and
+          reliable delivery services, so you can enjoy the convenience of
+          doorstep delivery without any hassle or delay.
+        </p>
+        <p>
+          We invite you to join the GrocHub family and experience the future
+          of grocery shopping. Welcome to a fresher, easier way to shop.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
